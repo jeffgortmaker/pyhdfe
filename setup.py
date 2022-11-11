314057b54a77efee9b0c16df2d773c4de9f5767c
Jeff Gortmaker <jeff@jeffgortmaker.com>
2022-11-11 09:53:51 -0500

    DOC: pin versions for doc builds

--- setup.py
+++ setup.py
@@ -21,7 +21,10 @@ setup(
     install_requires=read('requirements.txt').splitlines(),
     extras_require={
         'tests': ['pytest', 'pytest-xdist'],
-        'docs': ['sphinx', 'ipython', 'astunparse', 'sphinx-rtd-theme', 'nbsphinx', 'jupyter_client'],
+        'docs': [
+            'sphinx==2.0.0', 'ipython', 'astunparse', 'sphinx-rtd-theme==0.4.3', 
+            'nbsphinx==0.5.0', 'jinja2~=2.11', 'docutils==0.17',
+        ],
     },
     include_package_data=True,
     description="High dimensional fixed effect absorption with Python 3",
