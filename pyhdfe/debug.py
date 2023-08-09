1716fab2fd90fd70c2b3457593da2c815c646884
Alexander Fischer <alexander-fischer1801@t-online.de>
2023-08-09 21:08:58 +0200

    delete debug.py

--- pyhdfe/debug.py
+++
@@ -1,9 +0,0 @@
-import pyhdfe
-import numpy as np
-
-N = 1000
-X = np.random.normal(0, 1, 2*N).reshape((N, 2))
-g = np.random.choice(["a", "b", "c", "d", "e"], N).reshape((N, 1))
-weights = np.random.uniform(0, 1, N).reshape((N, 1))
-
-within = pyhdfe.create(g, residualize_method="dummy").residualize(X, weights = weights)
